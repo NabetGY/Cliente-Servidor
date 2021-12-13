a535dfe50f847017d96a9b318724c1144395d794
nabet <yoan.lopez@utp.edu.co>
2021-12-13 16:05:05 -0500

    server3 add

--- anillo/serv/server2.py
+++ anillo/serv/server2.py
@@ -4,20 +4,26 @@ import json
 import hashlib
 import os
 
-ipServer = os.popen('ip addr show ztc3q6fy2x | grep "\<inet\>" | awk \'{ print $2 }\' | awk -F "/" \'{ print $1 }\'').read().strip()
+ipServer = (
+    os.popen(
+        "ip addr show ztc3q6fy2x | grep \"\<inet\>\" | awk '{ print $2 }' | awk -F \"/\" '{ print $1 }'"
+    )
+    .read()
+    .strip()
+)
 
 
 def sha1Serv(serverName):
     hash_object = hashlib.sha1(serverName.encode())
     name = hash_object.hexdigest()
-    nameAsNum=int(name,16)
+    nameAsNum = int(name, 16)
     return nameAsNum
 
 
 parameters = {
-    'id': str(sha1Serv(str(ipServer))),
-    'opcion': sys.argv[1],
-    'ip': sys.argv[2] if sys.argv[1] != '--bootstrap' else None
+    "id": str(sha1Serv(str(ipServer))),
+    "opcion": sys.argv[1],
+    "ip": sys.argv[2] if sys.argv[1] != "--bootstrap" else None,
 }
 
 
@@ -27,145 +33,133 @@ parameters = {
 BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
 SIZE = 512
 
+
 def megaToSha(megabyte):
-    hash_object = hashlib.sha1( megabyte )
+    hash_object = hashlib.sha1(megabyte)
     name = hash_object.hexdigest()
-    nameAsNum = int( name, 16 )
+    nameAsNum = int(name, 16)
     return nameAsNum
 
 
 ###########################################################################################################################################
 
-def status( rango, predecesor, sucesor ):
-    print('mi nuevo rango :'+rango.toStr())
-    print('sucesor :'+sucesor)
-    print('predecesor :'+predecesor)
-    print('\n')
+
+def status(rango, predecesor, sucesor):
+    print("mi nuevo rango :" + rango.toStr())
+    print("sucesor :" + sucesor)
+    print("predecesor :" + predecesor)
+    print("\n")
 
 
 ######################################################################################################################
 
-def menu( server, rango, predecesor, sucesor ):
 
-    while( True ):
+def menu(server, rango, predecesor, sucesor):
+
+    while True:
 
         data = server.recv_json()
         opciones = json.loads(data)
 
-        if ( opciones.get( 'opcion' ) == 'ubicame!' ):
+        if opciones.get("opcion") == "ubicame!":
 
-            idServer = opciones.get( 'idServer' )
+            idServer = opciones.get("idServer")
 
-            pertenece = rango.member( int(idServer) )
+            pertenece = rango.member(int(idServer))
 
-            if ( pertenece ):
+            if pertenece:
 
                 serverData = {
-                    'sucesor': ipServer,
-                    'predecesor': predecesor,
-                    'opcion' : 'loUbico'
+                    "sucesor": ipServer,
+                    "predecesor": predecesor,
+                    "opcion": "loUbico",
                 }
-                
-                data = json.dumps( serverData )
-                server.send_json( data )
-                
-                rango = Range( int(idServer), int(parameters.get('id')) )
-                predecesor = opciones.get('ipServer')
-                status( rango, predecesor, sucesor )
+
+                data = json.dumps(serverData)
+                server.send_json(data)
+
+                rango = Range(int(idServer), int(parameters.get("id")))
+                predecesor = opciones.get("ipServer")
+                status(rango, predecesor, sucesor)
 
             else:
-                infoSucesor = {
-                    'sucesor': sucesor,
-                    'opcion' : 'noTeUbique'
-                }
+                infoSucesor = {"sucesor": sucesor, "opcion": "noTeUbique"}
 
                 data = json.dumps(infoSucesor)
                 server.send_json(data)
-        
-        elif ( opciones.get( 'opcion' ) == 'newSucesor' ):
-            
-            sucesor = opciones.get( 'sucesor' )
-            server.send_string( parameters.get('id') )
-            
-            status( rango, predecesor, sucesor )
-            
-            
-        elif ( opciones.get( 'opcion' ) == 'upload' ):
-            
-            server.send_string('ok') 
+
+        elif opciones.get("opcion") == "newSucesor":
+
+            sucesor = opciones.get("sucesor")
+            server.send_string(parameters.get("id"))
+
+            status(rango, predecesor, sucesor)
+
+        elif opciones.get("opcion") == "upload":
+
+            server.send_string("ok")
             mbyte = server.recv_multipart()
-            
-            hashMb = megaToSha( mbyte[0] )
-                        
+
+            hashMb = megaToSha(mbyte[0])
+
             if rango.member(hashMb):
-                
-                fileName = str( hashMb )
-                with open( fileName, 'ab' ) as file:
-                    file.write( mbyte[0] )
-                    resp = {
-                        'opcion' : 'exito'
-                    }
+
+                fileName = str(hashMb)
+                with open(fileName, "ab") as file:
+                    file.write(mbyte[0])
+                    resp = {"opcion": "exito"}
 
                     data = json.dumps(resp)
                     server.send_json(data)
-                    
+
             else:
-                infoSucesor = {
-                    'sucesor': sucesor,
-                    'opcion' : 'noEsMio'
-                }
+                infoSucesor = {"sucesor": sucesor, "opcion": "noEsMio"}
 
                 data = json.dumps(infoSucesor)
                 server.send_json(data)
 
-        elif ( opciones.get( 'opcion' ) == 'download' ):
-            
-            
-            hashMb = opciones.get('fileName')
-        
-            
+        elif opciones.get("opcion") == "download":
+
+            hashMb = opciones.get("fileName")
+
             archivo = str(hashMb)
-            
+
             if rango.member(hashMb):
-                with open(archivo, 'rb') as file:
+                with open(archivo, "rb") as file:
                     mByte = file.read()
-                    
-                    infoSucesor = {
-                    'opcion' : 'esMio'
-                    }
-                    
+
+                    infoSucesor = {"opcion": "esMio"}
+
                     data = json.dumps(infoSucesor)
                     server.send_json(data)
-                    
+
                     server.recv_string()
-                    
+
                     server.send_multipart([mByte])
-                    
+
             else:
-                infoSucesor = {
-                    'sucesor': sucesor,
-                    'opcion' : 'noEsMio'
-                }
-                
+                infoSucesor = {"sucesor": sucesor, "opcion": "noEsMio"}
+
                 data = json.dumps(infoSucesor)
                 server.send_json(data)
-                
+
 
 #############################################################################################3
+
 
 def bootstrap():
     context = zmq.Context()
     serverOne = context.socket(zmq.REP)
     miSucesor = ipServer
     miPredecesor = ipServer
-    miRango = Range(0, pow(2,160))
+    miRango = Range(0, pow(2, 160))
 
-    serverOne.bind('tcp://'+ipServer+':8001')
-    print('Servidor inicial...')
-    print('Mi rango es: '+miRango.toStr())
-    print('\n')
+    serverOne.bind("tcp://" + ipServer + ":8001")
+    print("Servidor inicial...")
+    print("Mi rango es: " + miRango.toStr())
+    print("\n")
 
-    menu( serverOne, miRango, miPredecesor, miSucesor )
+    menu(serverOne, miRango, miPredecesor, miSucesor)
 
 
 #############################################################################################3
@@ -179,96 +173,96 @@ def connect():
     miRango = None
 
     cliente = context.socket(zmq.REQ)
-    cliente.connect( 'tcp://'+parameters.get('ip') )
-    print('Soy nuevo y trato de ubicarme!...')
-    print('\n')
-    
+    cliente.connect("tcp://" + parameters.get("ip"))
+    print("Soy nuevo y trato de ubicarme!...")
+    print("\n")
+
     info = {
-        'opcion': 'ubicame!',
-        'idServer': parameters.get('id'),
-        'ipServer': ipServer,
+        "opcion": "ubicame!",
+        "idServer": parameters.get("id"),
+        "ipServer": ipServer,
     }
 
     infoJSON = json.dumps(info)
 
-    while(True):
+    while True:
 
-        cliente.send_json( infoJSON )
+        cliente.send_json(infoJSON)
         data = cliente.recv_json()
         opciones = json.loads(data)
 
-        if ( opciones.get( 'opcion' ) == 'loUbico' ):
-            
-            miSucesor = opciones.get( 'sucesor' )
-            miPredecesor = opciones.get( 'predecesor' )
-            
+        if opciones.get("opcion") == "loUbico":
+
+            miSucesor = opciones.get("sucesor")
+            miPredecesor = opciones.get("predecesor")
+
             cliente = context.socket(zmq.REQ)
-            cliente.connect('tcp://'+miPredecesor+':8001')
+            cliente.connect("tcp://" + miPredecesor + ":8001")
 
             newSucesor = {
-                'opcion' : 'newSucesor',
-                'sucesor': ipServer,
+                "opcion": "newSucesor",
+                "sucesor": ipServer,
             }
 
-            newSucesorJSON = json.dumps(newSucesor)   
-            cliente.send_json( newSucesorJSON )
-            
+            newSucesorJSON = json.dumps(newSucesor)
+            cliente.send_json(newSucesorJSON)
+
             idSucesor = cliente.recv_string()
-            miRango = Range( int(idSucesor), int(parameters.get('id')) )
+            miRango = Range(int(idSucesor), int(parameters.get("id")))
             cliente.close()
-            
+
             server = context.socket(zmq.REP)
-            server.bind('tcp://'+ipServer+':8001')
-            
-            status( miRango, miPredecesor, miSucesor )
+            server.bind("tcp://" + ipServer + ":8001")
+
+            status(miRango, miPredecesor, miSucesor)
             break
-        
-        elif ( opciones.get( 'opcion' ) == 'noTeUbique' ):
-            
-            siguiente = opciones.get( 'sucesor' )
-            
+
+        elif opciones.get("opcion") == "noTeUbique":
+
+            siguiente = opciones.get("sucesor")
+
             cliente = context.socket(zmq.REQ)
-            cliente.connect('tcp://'+siguiente+':8001')
-    
-    menu( server, miRango, miPredecesor, miSucesor )
+            cliente.connect("tcp://" + siguiente + ":8001")
+
+    menu(server, miRango, miPredecesor, miSucesor)
+
 
 #############################################################################################3
 
+
 class Range:
-    def __init__(self,lb,ub):
+    def __init__(self, lb, ub):
         self.lb = lb
         self.ub = ub
-    
+
     def isFirst(self):
         return self.lb > self.ub
-    
+
     def member(self, id):
         if self.isFirst():
-            return (id >= self.lb and id < 1<<160) or (id >= 0 and id < self.ub )
+            return (id >= self.lb and id < 1 << 160) or (id >= 0 and id < self.ub)
         else:
             return id >= self.lb and id < self.ub
-    
+
     def toStr(self):
         if self.isFirst():
-            return '[' + str(self.lb) + ' , 2**160) U [' + '0 , ' +  str(self.ub) + ')'
+            return "[" + str(self.lb) + " , 2**160) U [" + "0 , " + str(self.ub) + ")"
         else:
-            return '[' + str (self.lb) + ' , ' + str(self.ub) + ')'
-
-
-
+            return "[" + str(self.lb) + " , " + str(self.ub) + ")"
 
 
 #############################################################################################3
 
 
 def main():
-    if ( parameters.get('opcion') == '--bootstrap' ):
+    if parameters.get("opcion") == "--bootstrap":
         bootstrap()
-    elif ( parameters.get('opcion') == '--connect' ):
+    elif parameters.get("opcion") == "--connect":
         connect()
+
 
 #############################################################################################3
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ anillo/serv/server3.py
@@ -0,0 +1,270 @@
+import zmq
+import sys
+import json
+import hashlib
+import os
+
+#############################################################################################3
+
+# BUF_SIZE is totally arbitrary, change for your app!
+BUF_SIZE = 65536   # lets read stuff in 64kb chunks!
+SIZE = 512
+
+
+class Range:
+
+    def __init__( self, lb, ub ):
+        self.lb = lb
+        self.ub = ub
+
+    def isFirst( self ):
+        return self.lb > self.ub
+
+    def member( self, id ):
+        if self.isFirst():
+            return ( id >= self.lb and
+                     id < 1 << 160 ) or ( id >= 0 and id < self.ub )
+        else:
+            return id >= self.lb and id < self.ub
+
+    def toStr( self ):
+        if self.isFirst():
+            return "[" + str( self.lb ) + " , 2**160) U [" + "0 , " + str(
+                self.ub
+            ) + ")"
+        else:
+            return "[" + str( self.lb ) + " , " + str( self.ub ) + ")"
+
+
+###########################################################################################################################################
+
+
+def status( rango, predecesor, sucesor ):
+    print( "mi nuevo rango :" + rango.toStr() )
+    print( "sucesor :" + sucesor )
+    print( "predecesor :" + predecesor )
+    print( "\n" )
+
+
+######################################################################################################################
+def megaToSha( megabyte ):
+    hash_object = hashlib.sha1( megabyte )
+    name = hash_object.hexdigest()
+    nameAsNum = int( name, 16 )
+    return nameAsNum
+
+
+####################################################################################################################3
+
+
+def sha1Serv( serverName ):
+    hash_object = hashlib.sha1( serverName.encode() )
+    name = hash_object.hexdigest()
+    nameAsNum = int( name, 16 )
+    return nameAsNum
+
+
+class Server:
+
+    def __init__( self, ipConnection, ConnectionType ):
+        self.ip = ''
+        self.setIP()
+        self.id = str( sha1Serv( str( self.ip ) ) )
+        self.socket = None
+        self.successor = ''
+        self.predeccessor = ''
+        self.range = None
+        self.bootstrap_or_connect( ConnectionType, ipConnection )
+
+    def setIP( self ):
+        self.ip = (
+            os.popen(
+                "ip addr show ztc3q6fy2x | grep \"\<inet\>\" | awk '{ print $2 }' | awk -F \"/\" '{ print $1 }'"
+            ).read().strip()
+        )
+
+    def bootstrap_or_connect( self, ConnectionType, ipConnection ):
+        if ConnectionType == 'bootstrap':
+            self.bootstrap()
+        else:
+            self.connect( ConnectionType, ipConnection )
+
+    def bootstrap( self ):
+
+        self.successor = self.ip
+        self.predeccessor = self.ip
+        self.range = Range( 0, pow( 2, 160 ) )
+        context = zmq.Context()
+        self.socket = context.socket( zmq.REP )
+        self.socket.bind( "tcp://" + self.ip + ":8001" )
+        print( "Servidor inicial..." )
+        print( "Mi rango es: " + self.range.toStr() )
+        print( "\n" )
+
+    def connect( self, ipConnection ):
+        context = zmq.Context()
+
+        client = context.socket( zmq.REQ )
+        client.connect( "tcp://" + ipConnection )
+        print( "Soy nuevo y trato de ubicarme!..." )
+        print( "\n" )
+
+        info = {
+            "opcion": "ubicame!",
+            "idServer": self.id,
+            "ipServer": self.ip,
+        }
+
+        infoJSON = json.dumps( info )
+
+        while True:
+
+            cliente.send_json( infoJSON )
+            data = cliente.recv_json()
+            opciones = json.loads( data )
+
+            if opciones.get( "opcion" ) == "loUbico":
+
+                self.successor = opciones.get( "sucesor" )
+                self.predecessor = opciones.get( "predecesor" )
+
+                cliente = context.socket( zmq.REQ )
+                cliente.connect( "tcp://" + self.predecessor + ":8001" )
+
+                newSucesor = {
+                    "opcion": "newSucesor",
+                    "sucesor": self.ip,
+                }
+
+                newSucesorJSON = json.dumps( newSucesor )
+                cliente.send_json( newSucesorJSON )
+
+                idSucesor = cliente.recv_string()
+                self.range = Range( int( idSucesor ), int( self.id ) )
+                cliente.close()
+
+                self.socket = context.socket( zmq.REP )
+                self.socket.bind( "tcp://" + self.ip + ":8001" )
+
+                status( self.range, self.predecessor, self.successor )
+                break
+
+            elif opciones.get( "opcion" ) == "noTeUbique":
+
+                siguiente = opciones.get( "sucesor" )
+
+                cliente = context.socket( zmq.REQ )
+                cliente.connect( "tcp://" + siguiente + ":8001" )
+
+    def menu( self ):
+
+        while True:
+
+            data = self.socket.recv_json()
+            opciones = json.loads( data )
+
+            if opciones.get( "opcion" ) == "ubicame!":
+
+                idServer = opciones.get( "idServer" )
+
+                pertenece = self.range.member( int( idServer ) )
+
+                if pertenece:
+
+                    serverData = {
+                        "sucesor": self.successor,
+                        "predecesor": self.predecessor,
+                        "opcion": "loUbico",
+                    }
+
+                    data = json.dumps( serverData )
+                    self.socket.send_json( data )
+
+                    self.range = Range( int( idServer ), int( self.id ) )
+                    self.predecessor = opciones.get( "ipServer" )
+                    status( self.range, self.predecessor, self.successor )
+
+                else:
+                    infoSucesor = {
+                        "sucesor": self.successor, "opcion": "noTeUbique"
+                    }
+
+                    data = json.dumps( infoSucesor )
+                    self.socket.send_json( data )
+
+            elif opciones.get( "opcion" ) == "newSucesor":
+
+                self.successor = opciones.get( "sucesor" )
+                self.socket.send_string( self.id )
+
+                status( self.range, self.predecessor, self.successor )
+
+            elif opciones.get( "opcion" ) == "upload":
+
+                self.socket.send_string( "ok" )
+                mbyte = self.socket.recv_multipart()
+
+                hashMb = megaToSha( mbyte[ 0 ] )
+
+                if self.range.member( hashMb ):
+
+                    fileName = str( hashMb )
+                    with open( fileName, "ab" ) as file:
+                        file.write( mbyte[ 0 ] )
+                        resp = { "opcion": "exito"}
+
+                        data = json.dumps( resp )
+                        self.socket.send_json( data )
+
+                else:
+                    infoSucesor = {
+                        "sucesor": self.successor, "opcion": "noEsMio"
+                    }
+
+                    data = json.dumps( infoSucesor )
+                    self.socket.send_json( data )
+
+            elif opciones.get( "opcion" ) == "download":
+
+                hashMb = opciones.get( "fileName" )
+
+                archivo = str( hashMb )
+
+                if self.range.member( hashMb ):
+                    with open( archivo, "rb" ) as file:
+                        mByte = file.read()
+
+                        infoSucesor = { "opcion": "esMio"}
+
+                        data = json.dumps( infoSucesor )
+                        self.socket.send_json( data )
+
+                        self.socket.recv_string()
+
+                        self.socket.send_multipart( [ mByte ] )
+
+                else:
+                    infoSucesor = {
+                        "sucesor": self.successor, "opcion": "noEsMio"
+                    }
+
+                    data = json.dumps( infoSucesor )
+                    self.socket.send_json( data )
+
+
+#############################################################################################
+
+
+def main():
+
+    ConnectionType = sys.argv[ 1 ]
+    ipConnection = sys.argv[ 2 ] if sys.argv[ 1 ] != "--bootstrap" else None
+
+    server = Server( ipConnection, ConnectionType )
+    server.menu()
+
+
+#############################################################################################3
+
+if __name__ == "__main__":
+    main()
